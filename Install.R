38dbff2394df9ee20d9bb4c19b3bdf41f49c7cb4
Ewan Oleghe <ewan.mails@gmail.com>
2026-04-05 09:33:44 -0400

    first commit

---
+++ Install.R
@@ -0,0 +1,13 @@
+# if needed, installl "remotes" package:
+# install.packages("remotes")
+remotes::install_github("gadenbuie/xaringanthemer")
+remotes::install_github('yihui/xaringan')
+
+install.packages("devtools", dependencies = TRUE)
+library(devtools)
+
+install.packages("devtools", dependencies = TRUE)
+devtools::install_github("yihui/xaringan")
+
+
+install.packages("xaringanthemer", repos = c('https://gadenbuie.r-universe.dev', 'https://cloud.r-project.org'))
